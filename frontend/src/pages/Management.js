5a4e52e0d8c72bf529d9e7ff9b7035930da6bdf1
Girish Rawat <emailgirishrawat@gmail.com>
2020-05-01 19:31:13 -0700

    added import statements

--- frontend/src/pages/Management.js
+++ frontend/src/pages/Management.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Alert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core/styles";
 
 const Management = ({appUser, setAppUser}) => {
   if (appUser !== 'admin') {
