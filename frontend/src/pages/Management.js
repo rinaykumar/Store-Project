391bb8c136e49ce59e455f318df69ebc8096f181
Girish Rawat <emailgirishrawat@gmail.com>
2020-05-06 20:01:33 -0700

    Added useStyles to management page

--- frontend/src/pages/Management.js
+++ frontend/src/pages/Management.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import '../App.css';
-import axios from 'axios';
+import React from "react";
+import { Redirect, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Alert from "@material-ui/lab/Alert";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const Management = ({appUser, setAppUser}) => {
-  const [item, setItem] = React.useState('');
-  const [price, setPrice] = React.useState(''); 
+import "../App.css";
+import axios from "axios";
+
+const Management = ({ appUser, setAppUser }) => {
+  const [item, setItem] = React.useState("");
+  const [price, setPrice] = React.useState("");
   const [items, setItems] = React.useState([]);
-   
+
   const fetchItems = () => {
-    axios.get('/api/getAllItems')
+    axios
+      .get("/api/getAllItems")
       .then((res) => {
         console.log(res);
         setItems(res.data.items);
@@ -17,31 +28,33 @@ const Management = ({appUser, setAppUser}) => {
       .catch(console.log);
   };
 
-  const submitItem = () => { 
+  const submitItem = () => {
     console.log("From submitItem");
     console.log(item);
     console.log(price);
     const body = {
       item: item,
-      price: price
+      price: price,
     };
-    axios.post('/api/addItem', body)
-      .then(() => setItem(''))
-      .then(() => setPrice(''))
+    axios
+      .post("/api/addItem", body)
+      .then(() => setItem(""))
+      .then(() => setPrice(""))
       .then(() => fetchItems())
       .catch(console.log);
   };
 
-  const deleteItem = (itemName) => { 
+  const deleteItem = (itemName) => {
     console.log("From deleteItem");
     console.log(itemName);
     const body = {
       item: parseItem(itemName),
-      price: parsePrice(itemName)
+      price: parsePrice(itemName),
     };
-    axios.post('/api/deleteItem', body)
-      .then(() => setItem(''))
-      .then(() => setPrice(''))
+    axios
+      .post("/api/deleteItem", body)
+      .then(() => setItem(""))
+      .then(() => setPrice(""))
       .then(() => fetchItems())
       .catch(console.log);
   };
@@ -49,34 +62,50 @@ const Management = ({appUser, setAppUser}) => {
   const parseItem = (item) => {
     let obj = JSON.parse(item);
     return obj.item;
-  }
+  };
 
   const parsePrice = (item) => {
     let obj = JSON.parse(item);
     return obj.price;
-  }
+  };
 
   React.useEffect(() => {
     fetchItems();
   }, []);
 
-  if (appUser !== 'admin') {
-    return <Redirect to="/"/>;
+  if (appUser !== "admin") {
+    return <Redirect to="/" />;
   }
 
   return (
     <div>
+      <AppBar title="My App">
+        <Tabs>
+          <Tab label="Item 1" />
+          <Tab label="Item 2" />
+          <Tab label="Item 3" />
+          <Tab label="Item 4" />
+        </Tabs>
+      </AppBar>
       <div>
         <nav>
-          {appUser && <p>Welcome {appUser}</p>} 
-          <Link to="/store">Store</Link> 
+          {appUser && <p>Welcome {appUser}</p>}
+          <Link to="/store">Store</Link>
           <Link to="/logout">Logout</Link>
         </nav>
         <h1>Management Page</h1>
       </div>
       <div>
-        <input value={price} onChange={e => setPrice(e.target.value)} placeholder="Price"/>
-        <input value={item} onChange={e => setItem(e.target.value)} placeholder="Item Name"/>
+        <input
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Price"
+        />
+        <input
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Item Name"
+        />
         <button onClick={submitItem}>Add</button>
       </div>
       <div>
@@ -89,7 +118,7 @@ const Management = ({appUser, setAppUser}) => {
                 {parseItem(item)}
                 <button onClick={() => deleteItem(item)}>Delete</button>
               </div>
-            ); 
+            );
           })}
         </div>
       </div>
