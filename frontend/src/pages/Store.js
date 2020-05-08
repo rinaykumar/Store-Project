45934e18633bd34ec13513798642b2c886013117
Girish Rawat <emailgirishrawat@gmail.com>
2020-05-07 20:12:17 -0700

    updated look of transaction list

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -39,6 +39,13 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
 
   },
+
+  paperList2: {
+    height: "5ch",
+    width: "75ch",
+    display: "flex",
+    marginTop: "1em"
+  },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
@@ -96,21 +103,25 @@ const useStyles = makeStyles((theme) => ({
   totalLabel: {
     paddingTop: "1rem",
   },
-}));
 
-const Store = ({ appUser, setAppUser }) => {
+  totalPrice: {
+    paddingRight: "1em",
+    paddingTop: "1rem",
+  }
+}));
+const Store = ({appUser, setAppUser}) => {
   const classes = useStyles();
-  const [selectedItem, setSelectedItem] = React.useState("");
+  const [selectedItem, setSelectedItem] = React.useState('');
   const [items, setItems] = React.useState([]);
-  const [quantity, setQuantity] = React.useState("");
+  const [quantity, setQuantity] = React.useState('');
   const [cart, setCart] = React.useState([]);
   const [transactions, setTransactions] = React.useState([]);
   let itemsMap = new Map();
   let total = [];
+  let cartItemNames = [];
 
   const fetchItems = () => {
-    axios
-      .get("/api/getAllItems")
+    axios.get('/api/getAllItems')
       .then((res) => {
         console.log(res);
         setItems(res.data.items);
@@ -119,19 +130,16 @@ const Store = ({ appUser, setAppUser }) => {
   };
 
   const fetchCart = () => {
-    axios
-      .get("/api/getCart")
+    axios.get('/api/getCart')
       .then((res) => {
         console.log(res);
         setCart(res.data.cart);
-        //console.log(cart)
       })
       .catch(console.log);
   };
 
   const fetchTransactions = () => {
-    axios
-      .get("/api/getTransactions")
+    axios.get('/api/getTransactions')
       .then((res) => {
         console.log(res);
         setTransactions(res.data.transactions);
@@ -139,100 +147,100 @@ const Store = ({ appUser, setAppUser }) => {
       .catch(console.log);
   };
 
-  const addCart = (name, quant) => {
+  const addCart = (name, quant) => { 
     console.log("From addCart");
     console.log(name);
     console.log(quant);
     const body = {
       item: name,
       price: itemsMap.get(name),
-      quantity: quant,
+      quantity: quant
     };
-    axios
-      .post("/api/addCart", body)
-      .then(() => setSelectedItem(""))
-      .then(() => setQuantity(""))
+    axios.post('/api/addCart', body)
+      .then(() => setSelectedItem(''))
+      .then(() => setQuantity(''))
       .then(() => fetchCart())
       .catch(console.log);
   };
 
-  const deleteCart = (cartName) => {
+  const deleteCart = (cartName) => { 
     console.log("From deleteCart");
     const body = {
       item: parseCartItem(cartName),
       price: itemsMap.get(parseCartItem(cartName)),
-      quantity: parseCartQuantity(cartName),
+      quantity: parseCartQuantity(cartName)
     };
-    axios
-      .post("/api/deleteCart", body)
-      //.then(() => setItem(''))
-      //.then(() => setPrice(''))
+    axios.post('/api/deleteCart', body)
       .then(() => fetchCart())
       .catch(console.log);
   };
 
-  const addTransaction = (cartItems, cartTotal) => {
+  const addTransaction = (cartItems, cartTotal) => { 
     console.log("From addTransaction");
     console.log(cartItems);
     console.log(sumTotal(cartTotal));
     const body = {
-      items: cartItems,
+      items: cartItemNames,
       total: sumTotal(cartTotal),
     };
-    axios
-      .post("/api/addTransaction", body)
-      //.then(() => setSelectedItem(''))
-      //.then(() => setQuantity(''))
+    axios.post('/api/addTransaction', body)
       .then(() => fetchTransactions())
       .catch(console.log);
   };
 
   const parseItem = (item) => {
     let obj = JSON.parse(item);
-    itemsMap.set(obj.item, obj.price);
+    itemsMap.set(obj.item, obj.price)
     console.log(itemsMap);
     return obj.item;
-  };
+  }
 
   const parsePrice = (selectedItem) => {
     console.log("From parsePrice");
     return itemsMap.get(selectedItem);
-  };
+  }
 
   const parseCartItem = (cartItem) => {
     let obj = JSON.parse(cartItem);
+    cartItemNames.push(obj.item);
     return obj.item;
-  };
+  }
 
   const parseCartQuantity = (cartItem) => {
     let obj = JSON.parse(cartItem);
     return obj.quantity;
-  };
+  }
 
   const parseCartSubtotal = (cartItem) => {
     let obj = JSON.parse(cartItem);
     total.push(obj.subtotal);
     return obj.subtotal;
-  };
+  }
+
+  const parseTransaction = (transactionItem) => {
+    console.log("From parseTransaction");
+    let obj = JSON.parse(transactionItem);
+    let itemString = obj.items.toString();
+    console.log(itemString);
+    return itemString;
+  }
+
+  const parseTransactionTotal = (transactionItem) => {
+    let obj = JSON.parse(transactionItem);
+    return obj.total;
+  }
+  
 
   const sumTotal = (total) => {
-    let cartTotal = total.reduce(function (a, b) {
-      return a + b;
-    }, 0);
-    return cartTotal;
-  };
-
+    let cartTotal = total.reduce(function(a, b) {return a+b;}, 0);
+    return cartTotal
+  }
+  
   React.useEffect(() => {
     fetchItems();
     fetchCart();
     fetchTransactions();
   }, []);
-
-  /* Only logged in users can access
-  if (!appUser) {
-    return <Redirect to="/"/>;
-  }
-  */
 
   return (
     <div>
@@ -360,13 +368,38 @@ const Store = ({ appUser, setAppUser }) => {
           </Button>
         </form>
 
+        <div>Transaction History
         <div>
-          Transaction History:
-          <div>
-            {transactions.map((transactionItem) => {
-              return <div class="items-item">{transactionItem}</div>;
-            })}
-          </div>
+          {transactions.map((transactionItem) => {
+            return (
+
+
+<Paper elevation={3} className={classes.paperList2}>
+<Grid
+  justify="space-between" // Add it here :)
+  container
+  spacing={24}
+>
+  <Grid item>
+    <div className={classes.itemName}>
+    Items bought: <b>{parseTransaction(transactionItem)}</b> 
+
+    </div>
+  </Grid>
+
+  <Grid item>
+    <div className={classes.totalPrice}>
+    Total: <b>${parseTransactionTotal(transactionItem)}</b> 
+    </div>
+  </Grid>
+
+</Grid>
+</Paper>
+
+            );
+          })}
+        </div> 
+
         </div>
       </Container>
     </div>
